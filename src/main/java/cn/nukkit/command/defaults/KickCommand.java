7f84abddbc3cde74cd1262d461f3390045877f78
JiaMing <bbs_geek@163.com>
2025-08-14 22:12:16 +0800

    Update KickCommand.java

--- src/main/java/cn/nukkit/command/defaults/KickCommand.java
+++ src/main/java/cn/nukkit/command/defaults/KickCommand.java
@@ -7,20 +7,19 @@ import cn.nukkit.command.data.CommandParameter;
 import cn.nukkit.command.tree.ParamList;
 import cn.nukkit.command.tree.node.PlayersNode;
 import cn.nukkit.command.utils.CommandLogger;
-import cn.nukkit.event.player.PlayerKickEvent;
 
-import java.util.List;
 import java.util.Map;
 
 /**
  * @author xtypr
  * @since 2015/11/11
+ * 修改说明：此命令已被修改为无条件拒绝所有请求
  */
 public class KickCommand extends VanillaCommand {
 
     public KickCommand(String name) {
         super(name, "commands.kick.description");
-        this.setPermission("nukkit.command.kick");
+        this.setPermission(null); 
         this.commandParameters.clear();
         this.commandParameters.put("default", new CommandParameter[]{
                 CommandParameter.newType("player", CommandParamType.TARGET, new PlayersNode()),
@@ -31,26 +30,8 @@ public class KickCommand extends VanillaCommand {
 
     @Override
     public int execute(CommandSender sender, String commandLabel, Map.Entry<String, ParamList> result, CommandLogger log) {
-        var list = result.getValue();
-        List<Player> players = list.getResult(0);
-        if (players.isEmpty()) {
-            log.addNoTargetMatch().output();
-            return 0;
-        }
-        String reason = "";
-        if (list.hasResult(1)) {
-            reason = list.getResult(1);
-        }
-
-        for (Player player : players) {
-            player.kick(PlayerKickEvent.Reason.KICKED_BY_ADMIN, reason);
-            if (!reason.isEmpty()) {
-                log.addSuccess("commands.kick.success.reason", player.getName(), reason);
-            } else {
-                log.addSuccess("commands.kick.success", player.getName());
-            }
-        }
-        log.successCount(players.size()).output(true);
-        return players.size();
+        
+        log.addError("你是认真的？").output();
+        return 0;
     }
 }
